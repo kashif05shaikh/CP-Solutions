99659ca59a342f8562de94c5727380cf4638bdc5
Kashif Shaikh <236854170+kashif05shaikh@users.noreply.github.com>
2026-08-21 23:02:23 +0530

    Add Bag-of-Tokens (two-pointers)

---
+++ leetcode/two-pointers/948-Bag-of-Tokens.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+
+        int i = 0, j = tokens.size() - 1;
+        int score = 0, ans = 0;
+
+        while (i <= j) {
+            if (power >= tokens[i]) {
+                power -= tokens[i];
+                score++;
+                ans = max(ans, score);
+                i++;
+            }
+            else if (score > 0) {
+                power += tokens[j];
+                score--;
+                j--;
+            }
+            else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
