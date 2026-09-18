1ed77a54dcb8f93a0e40ff52abc43245e7cda608
Kashif Shaikh <236854170+kashif05shaikh@users.noreply.github.com>
2026-09-18 19:13:05 +0530

    Add Moo Language School (800)

---
+++ codeforces/800/2259A - Moo Language School.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i += k) {
+            bool zero = false;
+
+            for (int j = i; j < i + k; j++) {
+                if (s[j] == '0') {
+                    zero = true;
+                    break;
+                }
+            }
+
+            if (!zero) cnt++;
+        }
+
+        cout << cnt << '\n';
+    }
+}
